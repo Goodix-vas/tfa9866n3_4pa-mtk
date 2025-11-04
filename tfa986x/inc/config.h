20ae9af52b68a84ec546dfc720855cc470dbf83c
jimmy <jkim@goodix.com>
2025-11-04 18:20:18 +0900

    initial commit the base code from the latest tfa9866_n3 driver for LSI platform

---
+++ tfa986x/inc/config.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014-2020 NXP Semiconductors, All Rights Reserved.
+ * Copyright 2020 GOODIX, All Rights Reserved.
+ */
+
+#ifndef __CONFIG_LINUX_KERNEL_INC__
+#define __CONFIG_LINUX_KERNEL_INC__
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/ctype.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/crc32.h>
+#include <linux/ftrace.h>
+#include <sound/pcm.h>
+
+#define _ASSERT(e)
+#define PRINT_ASSERT(e) {if ((e))\
+	pr_err("PrintAssert:%s (%s:%d) error code:%d\n",\
+	__func__, __FILE__, __LINE__, e); }
+
+#if !defined(TFA_STEREO_NODE)
+#define TFA_STEREO_NODE
+#endif
+#define TFA_PLATFORM_EXYNOS
+#define TFA_SUPPORT_NEW_DATALOGGER
+
+#endif /* __CONFIG_LINUX_KERNEL_INC__ */
+
